4472b3f4a66cddc805363eb9b3141a11bb2891b2
mamathanps <mamatha.k8050@gmail.com>
2020-12-27 21:16:35 +0530

    reactapp commit

---
+++ src/Clcomponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Clcomponent extends React.Component {
+    render () {
+        return (
+            <p>
+                Components are independent and reusable bits of code. They serve the same purpose as JavaScript functions, but work in isolation and return HTML via a render() function.
+            </p>
+        );
+    }
+}
+
+
+export default Clcomponent;
---
+++ src/Fcomponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Fcomponent () {
+    return (
+        <h1>
+            Hai, Functional Component How are you.....!!!
+        </h1>
+    );
+}
+
+export default Fcomponent;
